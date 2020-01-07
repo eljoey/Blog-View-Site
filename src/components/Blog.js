56a08c06854bd679af736f42d6465683788a3c76
Joe Hayden <eljoeyy@gmail.com>
2020-01-07 13:35:00 -0600

    basic outline of displaying single blogpost

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Blog = props => {
-  const [curBlog, setCurBlog] = useState([])
+  const [curBlog, setCurBlog] = useState({})
   const blogId = props.match.params.id
 
   useEffect(() => {
@@ -10,10 +10,38 @@ const Blog = props => {
       const res = await axios.get(`http://localhost:3000/api/blogs/${blogId}`)
       setCurBlog(res.data)
     }
-
     fetchCurBlog()
   }, [blogId])
-  return <div>{curBlog.title}</div>
+
+  const renderComments = () => {
+    if (!curBlog.comments || curBlog.comments.length === 0) {
+      return <div>No Comments</div>
+    } else {
+      console.log('blog', curBlog.comments)
+      return (
+        <>
+          <h4>Comments</h4>
+          <div>
+            {curBlog.comments.map(comment => {
+              return <div key={comment._id}>{comment.text}</div>
+            })}
+          </div>
+        </>
+      )
+    }
+  }
+
+  return (
+    <div>
+      <h3>{curBlog.title}</h3>
+      <div>{curBlog.content}</div>
+      <div>
+        <span>{curBlog.likes} likes </span>
+        <span>{curBlog.dislikes} dislikes </span>
+      </div>
+      <div>{renderComments()}</div>
+    </div>
+  )
 }
 
 export default Blog
